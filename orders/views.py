e05daa146bd3ff1e5f99f1644fe77bf8f73a98cb
root <root@Oluwafemi.localdomain>
2020-12-30 23:10:18 +0100

    place and retrieve orders

--- orders/views.py
+++ orders/views.py
@@ -10,7 +10,6 @@ from plants.models import Plants
 from .serializers import OrderSerializer
 from accounts import permissions
 
-
 class OrderView(generics.CreateAPIView):
     serializer_class = OrderSerializer
     permission_classes = (permissions.LoggedInPermission, permissions.UserIsUserPermission)
@@ -20,9 +19,29 @@ class OrderView(generics.CreateAPIView):
         ordered_by = self.request.user
         return serializer.save(ordered_by=ordered_by)
 
-
 class OrderListView(generics.ListAPIView):
     queryset = Orders.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (permissions.LoggedInPermission, permissions.UserIsNurseryPermission)
     authentication_class = (TokenAuthentication, )
+    
+    # def get(self, request):
+    #     user = request.user.id
+    #     orders = Orders.objects.filter(nursery=user)
+
+    #     store = {}
+        
+    #     for item in orders:
+    #         store['price'] = item.id
+    #         store['plants'] = item.plants
+    #         store['user'] = item.ordered_by_id
+        
+    #     print(store)
+
+    #     # data = {
+    #     #     "Plants Ordered" : price,
+    #     #     "Number of order" : plants,
+    #     #     "User" : user
+    #     # }
+
+    #     return Response(data, status=status.HTTP_200_OK)
